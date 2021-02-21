be6ddf960d47c05cb0d68acdcc1bd92b301c48a9
Andreas Adoni <adoni.andreas@cinetpay.com>
2021-02-21 20:22:27 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Im gonna be the greatest alive!'));
+app.get('/', (req, res) => res.send('GREATEST ALIVE!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
